3f7ae91dee7bc2c4d52375aff957d7c2c0d2cb7f
davisl24 <davislocs135@gmail.com>
2026-09-20 22:28:24 +0300

    Add app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"Skin & Sculpt | Estētiskās kosmetoloģijas centrs Tukumā",description:"Mūsdienīgas sejas un ķermeņa procedūras ādas kvalitātei, figūrai un pašsajūtai."};
+export default function RootLayout({children}){return <html lang="lv"><body>{children}</body></html>}
